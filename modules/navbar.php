4641dd4c1e6f7acb031df8a78a858ad09f31bdcc
Brainfogz <sinr0202@gmail.com>
2016-06-22 15:47:37 -0400

    changed some css

--- modules/navbar.php
+++ modules/navbar.php
@@ -2,9 +2,9 @@
   <div class="container align-middle">
     <ul class="navbar-list">
       <li class="navbar-item"><a class="navbar-link" href="/">Home</a></li>
-      <li class="navbar-item"><a class="navbar-link" href="/about.php">About Us</a></li>
+      <li class="navbar-item"><a class="navbar-link" href="/about.php">About</a></li>
       <li class="navbar-item"><a class="navbar-link" href="/faq.php">FAQ</a></li>
-      <li class="navbar-item"><a class="navbar-link" href="/contact.php">Contact Us</a></li>
+      <li class="navbar-item"><a class="navbar-link" href="/contact.php">Contact</a></li>
     </ul>
   </div>
 </nav>
